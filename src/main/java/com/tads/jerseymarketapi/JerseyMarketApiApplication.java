8170b31a2b6fe795c202955776cef480f10bdf25
lekramon <ramon.pinheiro20@gmail.com>
2023-03-15 01:05:42 -0300

    build: construção de dependências do escopo de projeto

---
+++ src/main/java/com/tads/jerseymarketapi/JerseyMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.tads.jerseymarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseyMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseyMarketApiApplication.class, args);
+	}
+
+}
